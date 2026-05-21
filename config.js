48ae6f6ce3d5f4db485fbcc02f7ce29cd1c08705
lauraarrieta4-eng <lauraarrieta4@gmail.com>
2026-05-21 08:24:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js - Conexión a Supabase (solo para el navegador)
 const _supabaseUrl = 'https://bzznyvzzwfqqxqddjqwh.supabase.co';
 const _supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6em55dnp6d2ZxcXhxZGRqcXdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMjQ2NDMsImV4cCI6MjA5MzkwMDY0M30.2WijLJ-wjL5o6d5NeVAow0VpNOpyxMYzX6Q69bd0o0I';
-const _supabase = Supabase.createClient(_supabaseUrl, _supabaseKey);
+const _supabase = supabase.createClient(_supabaseUrl, _supabaseKey);
